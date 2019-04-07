93ded7ce436e96cef75db54e0845e25721f2c474
whengk <44838011+whengk@users.noreply.github.com>
2019-04-07 13:26:28 +0800

    Update ServerBio.java

--- ServerBio.java
+++ ServerBio.java
@@ -26,17 +26,12 @@ public class ServerBio extends Thread{
             serverSocket = new ServerSocket(port); // bind   listen()
             System.out.println("服务器已启动 端口号："+ port);
             Socket socket;
-            //  通过无限循环监听客户连接
-            //如果没有客户端接入  将阻塞在accept操作上
             while(true){
                 socket = serverSocket.accept();
                 System.out.println("socket : "+ socket +"已连接");
-                //当有新的客户端接入是 汇之星下面的代码
-                //然后创建一个新的线程处理这条Socket 链路
                 new Thread((new ServerHandler(socket))).start();
             }
         }finally {
-            //一些必要的清理工作
             if(serverSocket != null){
                 System.out.println("服务器已关闭");
                 serverSocket.close();
