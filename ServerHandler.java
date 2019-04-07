03dc433172aa98904034f69e57c2e6d005286b3f
whengk <44838011+whengk@users.noreply.github.com>
2019-04-07 13:31:35 +0800

    Update ServerHandler.java

--- ServerHandler.java
+++ ServerHandler.java
@@ -4,14 +4,11 @@ import java.io.*;
 import java.net.Socket;
 
 public class ServerHandler implements Runnable{
-    //定义一个变量来保存 socket   --> 主线程监听到的  子线程有请求的一个 socket
     private Socket socket;
     public ServerHandler(Socket socket) {
         this.socket = socket;
     }
-
-    //在run方法中写具体需要做的事情 定义这个IO流和client 之间的通讯 这些东西用完之后要关闭
-
+    
     @Override
     public void run() {
         BufferedReader bufferedReader = null;
@@ -22,15 +19,12 @@ public class ServerHandler implements Runnable{
             String expression;
             String result;
             while(true){
-                //通过BufferedReader读取一行
-                //如果已经读到输入流尾部 返回null 退出循环
-                //如果得到非空值 就尝试计算结果并返回
                 if((expression = bufferedReader.readLine()) == null) break;
-                System.out.println("服务器收到 "+socket+" 消息： " + expression);
+                System.out.println("Server "+socket+" recerved the message： " + expression);
                 try{
                     result = String.valueOf(expression.hashCode());
                 }catch (Exception e){
-                    result = "计算错误" + e.getMessage();
+                    result = "calculation mistack" + e.getMessage();
                 }
                 bufferedWriter.write(result+"\n");
                 bufferedWriter.flush();
